86f05e8c1eb13e2fa983d6fb5a7b816889852a65
LIUKE <LIUKE@LIUKE-AE86>
2022-05-22 22:44:13 +0800

    upload

--- Code/Login.cs
+++ Code/Login.cs
@@ -19,28 +19,28 @@ namespace Code
 
         private void but_1_Click(object sender, EventArgs e)
         {
-            //暂时不使用数据库
+            //建立字符串数组
+            //string[] user_z = { "刘科", "xiaolan", "潘广瑞", "范雯心" };
+
+
+            //获取内容
             string name = User.Text;
             string pass = passBox2.Text;
-            //string a = "LIUKE";
 
-            //建立字符串数组
-            string[] user_z = { "刘科", "xiaolan", "潘广瑞", "范雯心" };
-            foreach(string user in user_z)
+            //验证User and password暂时不使用数据库
+            string user_u = "LIUKE";
+            string pass_w = "123456";
+            if (user_u == name && pass_w == pass)//此处待修改！！！
+             {
+                MessageBox.Show("登录成功");
+                Form form_index = new Form1();//创建实列
+                //form_index.Show();//登录窗口可以使用
+                this.Hide();//隐藏Login窗口
+                form_index.ShowDialog();
+            }
+            else
             {
-                //验证User and password
-                string b = "123456";
-                if (user == name && b == pass)//此处待修改！！！
-                {
-                    MessageBox.Show("登录成功");
-                    Form form_index = new Form1();
-                    //form_index.Show();//登录窗口可以使用
-                    form_index.ShowDialog();
-                }
-                else
-                {
-                    MessageBox.Show("输入错误");
-                }
+                 MessageBox.Show("输入错误");
             }
         }
 
